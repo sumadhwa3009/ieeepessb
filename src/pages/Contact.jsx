b443c1aa7b0029f47b6ae2ec85a36050c4f025a2
Sumadhwa V <sumadhwarao2005@gmail.com>
2025-10-03 22:51:31 +0530

    EmailJS Integration -2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import styles from "../styles/Contact.module.css";
 
 export default function Contact() {
